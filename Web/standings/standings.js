80c610036601946c9d5839ee9a6d2fffb11daee3
Alexander Stasiv <asavis@gmail.com>
2025-09-16 10:02:50 +0200

    work on adjustments started

--- Web/standings/standings.js
+++ Web/standings/standings.js
@@ -5,13 +5,15 @@
  *   <script type="text/javascript" src="https://cdn.sencha.com/ext/gpl/4.2.1/ext-all.js"></script>
  *   <script type="text/javascript" src="/standings/standings.js"></script>
  *
- *   <div id="extjs_grid"> 
+ *   <div id="extjs_grid"></div>
  */
 
 Ext.require([
     "Ext.grid.*",
     "Ext.data.*",
-    "Ext.util.*"
+    "Ext.util.*",
+    "Ext.panel.Panel",
+    "Ext.layout.container.Auto"
 ]);
 
 var StandingFileName = "/standings/standings.json";
@@ -23,106 +25,130 @@ var RanksIcons = [
 
 Ext.override(Ext.grid.View, { enableTextSelection: true });
 
-Ext.onReady(function() {
+Ext.onReady(function () {
 
     Ext.QuickTips.init();
 
-    Ext.define("StandingsModel",
-        {
-            extend: "Ext.data.Model",
-            fields: [
-                { name: "Id", type: "string" },
-                { name: "Name", type: "string" },
-                { name: "Rank", type: "int" },
-                { name: "Score", type: "float" },
-                { name: "LichessScore", type: "int" },
-                { name: "Results" }
-            ]
-        });
+    Ext.define("StandingsModel", {
+        extend: "Ext.data.Model",
+        fields: [
+            { name: "Id", type: "string" },
+            { name: "Name", type: "string" },
+            { name: "Rank", type: "int" },
+            { name: "Score", type: "float" },
+            { name: "LichessScore", type: "int" },
+            { name: "Results" } // Array
+        ]
+    });
 
-    const store = Ext.create("Ext.data.Store",
-        {
-            model: "StandingsModel",
-            proxy: {
-                type: "ajax",
-                url: StandingFileName,
-                reader: "json"
+    const store = Ext.create("Ext.data.Store", {
+        model: "StandingsModel",
+        proxy: {
+            type: "ajax",
+            url: StandingFileName,
+            reader: "json"
+        },
+        autoLoad: true
+    });
+
+    const grid = Ext.create("Ext.grid.Panel", {
+        store: store,
+
+        forceFit: true,
+
+        height: 594,
+        border: 1,
+
+        columns: [
+            {
+                text: "Місце",
+                sortable: true,
+                width: 60,
+                dataIndex: "Rank",
+                renderer: function (value, metaData, record) {
+                    const rank = record.data["Rank"];
+                    if (rank > 3) return value;
+                    return value + ' <img src="' + RanksIcons[rank - 1] + '" width="22" alt="rank ' + rank + '" style="float:right" />';
+                }
             },
-            autoLoad: true
-        });
+            {
+                text: "Команда",
+                flex: 1,
+                minWidth: 220,
+                sortable: true,
+                dataIndex: "Name",
+                renderer: function (value, metaData, record) {
+                    metaData.tdStyle = "white-space:nowrap;overflow:hidden;text-overflow:ellipsis;";
+                    const name = Ext.String.htmlEncode(value || "");
+                    metaData.tdAttr = 'data-qtip="' + name + '"';
+                    const id = encodeURIComponent(record.data["Id"] || "");
+                    return '<a href="https://lichess.org/team/' + id + '" target="_blank" rel="noopener">' + name + '</a>';
+                }
+            },
+            {
+                text: "Турніри",
+                width: 220, 
+                minWidth: 160,
+                sortable: false,
+                align: "left",
+                dataIndex: "Results",
+                renderer: function (value, metaData, record) {
+                    metaData.tdStyle = "white-space:nowrap;overflow:hidden;text-overflow:ellipsis;";
+                    if (!Ext.isArray(value) || value.length === 0) return "";
 
-    Ext.create("Ext.grid.Panel",
-        {
-            store: store,
-            columns: [
-                {
-                    text: "Місце",
-                    sortable: true,
-                    width: 60,
-                    dataIndex: "Rank",
-                    renderer: function(value, metaData, record, row, col, store, gridView) {
-                        const rank = record.data["Rank"];
-                        if (rank > 3) return value;
-                        return value + `<img src="${RanksIcons[rank - 1]}" width=22 align=right />`;
-                    }
-                },
-                {
-                    text: "Команда",
-                    flex: 1,
-                    sortable: true,
-                    dataIndex: "Name",
-                    renderer: function(value, metaData, record, row, col, store, gridView) {
-                        return `<a href="https://lichess.org/team/${record.data["Id"]}" target="_blank">${value}</a>`;
-                    }
-                },
-                {
-                    text: "Турніри",
-                    flex: 1,
-                    sortable: false,
-                    align: "left",
-                    dataIndex: "Results",
-                    renderer: function(value, metaData, record, row, col, store, gridView) {
-                        var txt = "";
-
-                        for (i = 0; i < value.length; i++) {
-                            if (i !== 0) txt += " + ";
-
-                            let tip = `[${value[i].Rank}] ${value[i].LeagueName} ${Ext.Date.format(
-                                Ext.Date.parse(value[i].TournamentDate, "c"),
-                                "d.m.Y")}`;
-
-                            tip = tip.replaceAll(" ", "&nbsp");
-                            txt += `<a href="https://lichess.org/tournament/${value[i].LichessTournamentId
-                                }" target="_blank" data-qtip="${tip}">${value[i].Score}</a>`;
+                    let txt = "";
+                    for (let i = 0; i < value.length; i++) {
+                        const item = value[i] || {};
+                        if (i !== 0) {
+                            txt += (item.ResultType === "adjustment" && typeof item.Value === "number")
+                                ? (item.Value >= 0 ? " + " : " ")
+                                : " + ";
+                        }
+                        if (item.ResultType === "adjustment") {
+                            const comment = Ext.String.htmlEncode(item.Comment || "");
+                            const valText = (typeof item.Value === "number")
+                                ? ((item.Value > 0 ? "+" : "") + item.Value)
+                                : Ext.String.htmlEncode(String(item.Value || ""));
+                            txt += '<span style="color:red;" data-qtip="' + comment + '">' + valText + '</span>';
+                            continue;
                         }
 
-                        return txt;
+                        const d = item.TournamentDate ? new Date(item.TournamentDate) : null;
+                        const dText = (d && !isNaN(d.getTime())) ? Ext.Date.format(d, "d.m.Y") : "";
+                        const league = Ext.String.htmlEncode(item.LeagueName || "");
+                        const rankText = (item.Rank != null) ? String(item.Rank) : "";
+                        const tip = ('[' + rankText + '] ' + league + ' ' + dText).split(' ').join('&nbsp;');
+                        const qtip = Ext.String.htmlEncode(tip);
+                        const lichessId = encodeURIComponent(item.LichessTournamentId || "");
+                        const scoreText = (item.Score != null) ? String(item.Score) : "";
+                        txt += '<a href="https://lichess.org/tournament/' + lichessId + '" target="_blank" rel="noopener" data-qtip="' + qtip + '">' + scoreText + '</a>';
                     }
-                },
-                {
-                    text: "Очки",
-                    sortable: true,
-                    width: 65,
-                    xtype: "numbercolumn",
-                    format: "0.0",
-                    align: "right",
-                    dataIndex: "Score"
-                },
-                {
-                    text: "Очки<br>Lichess",
-                    sortable: true,
-                    width: 65,
-                    align: "right",
-                    dataIndex: "LichessScore"
+                    return txt;
                 }
-            ],
-
-            height: 594,
-
-            viewConfig: {
-                stripeRows: true
             },
+            {
+                text: "Очки",
+                tooltip: "Сума очок з бонусами та штрафами",
+                sortable: true,
+                width: 66,
+                xtype: "numbercolumn",
+                format: "0.0",
+                align: "right",
+                dataIndex: "Score"
+            },
+            {
+                text: "Lichess",
+                tooltip: "Сума очок Lichess",
+                width: 66,
+                align: "right",
+                dataIndex: "LichessScore"
+            }
+        ],
 
-            renderTo: "extjs_grid"
-        });
+        viewConfig: {
+            stripeRows: true
+        },
+
+        renderTo: "extjs_grid"
+    });
 });
